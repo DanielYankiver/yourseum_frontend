9dfce938c393cebeec1eed9f6e09599fa4b9135c
Daniel <dyankiver@gmail.com>
2021-02-12 15:06:33 -0500

    Added CSS and finished MVP

--- src/components/ArtFavorites.js
+++ src/components/ArtFavorites.js
@@ -1,15 +1,24 @@
-import ArtCard from './ArtCard';
+// import ArtCard from './ArtCard';
 
 function ArtFavorites({artworks, handleFavorite}){
 
-    const favArt = artworks.map((a) => {
-        return <ArtCard 
-                    key={a.id} 
-                    artwork={a}
-                    handleFavorite={handleFavorite}
-                /> 
-    })
+    function handleStarred(artwork){
+        handleFavorite(artwork)
+    }
 
+    const favArt = artworks.map((artwork) => {
+        return (
+            <div>
+                <h1>{artwork.title}</h1>
+                <h2>{artwork.artist}</h2>
+                <h3>{artwork.year}</h3>
+                <img src={artwork.image}/>
+                {artwork.starred ? <button onClick={()=>handleStarred(artwork)}> ⭐️ </button> : <button onClick={()=>handleStarred(artwork)}> ☆ </button>}
+                <br/>
+            </div>
+            );
+    })
+    
     return (
         <div>
             <ul>
