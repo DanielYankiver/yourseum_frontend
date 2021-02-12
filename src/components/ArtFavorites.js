37fc48c3a96cf0f6160b8e913a81bc0c8491a543
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-02-12 12:13:23 -0500

    Update ArtFavorites.js

--- src/components/ArtFavorites.js
+++ src/components/ArtFavorites.js
@@ -1,1 +1,23 @@
+import ArtCard from './ArtCard';
 
+function ArtFavorites({artworks, handleFavorite}){
+
+    const favArt = artworks.map((a) => {
+        return <ArtCard 
+                    key={a.id} 
+                    artwork={a}
+                    handleFavorite={handleFavorite}
+                /> 
+    })
+
+    return (
+        <div>
+            <ul>
+                <h1>Favorites: </h1>   
+                {favArt}
+            </ul>
+        </div>
+    );
+}
+
+export default ArtFavorites;
